5eeb960168acebd8da4408529647b11257357a11
masterpnogpon <kira_songpon@hotmail.com>
2022-02-27 22:55:22 +0700

    Add File

---
+++ Assets/ChangeAnimator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimator : MonoBehaviour
+{
+    public void getAnimatorStand(){
+        this.GetComponent<Animator>().Play("Standing Torch Idle 03");
+    }
+    public void getAnimatorWalk(){
+        this.GetComponent<Animator>().Play("Walking");
+    }
+}
